8e4a4a092f3db7241663985efb8e07a52089d3fe
Jeffcc <jefftw@email.com>
2017-08-05 19:37:34 +0800

    加入註解

--- check_google_trend_feed.sh
+++ check_google_trend_feed.sh
@@ -4,13 +4,15 @@
 URL1="https://www.google.com.tw/trends/hottrends/atom/feed?pn=p12"
 identify="google.com.tw/trends/"
 
-STATUS1=`/usr/bin/wget --no-check-certificate -O /local/htdocs/gtrend_buf $URL1 2>&1|grep "not found\|error"`
+STATUS1=`/usr/bin/wget --no-check-certificate -O /local/htdocs/gtrend_buf $URL1 2>&1 | grep "not found\|error"`
 
-if [ "$STATUS1" ];then
+if [ "$STATUS1" ]; then
         echo 'Google trend download feed fail';
         echo $URL1;
 else
-        if [ "`/bin/cat /local/htdocs/gtrend_buf|grep \"$identify\"`" ];then
+        if [ "`/bin/cat /local/htdocs/gtrend_buf | grep \"$identify\"`" ]; then
+			#   中括號內  先用雙引號  再用 ``包起執行命令  grep 引數 前後要用\" 包起來
+		
                 /bin/cp -p /local/htdocs/gtrend_buf /local/htdocs/gtrend
                 chown cmadmin.cmadmin /local/htdocs/gtrend
                 /usr/bin/up2 /local/htdocs/gtrend
